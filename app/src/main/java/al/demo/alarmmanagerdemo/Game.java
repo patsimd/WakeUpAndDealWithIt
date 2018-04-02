0a547376b852b81bc2b60f4c3c6f7f52670028fd
fivocx <fivocx@gmail.com>
2018-04-02 18:54:10 -0400

    fix shits

--- app/src/main/java/al/demo/alarmmanagerdemo/Game.java
+++ app/src/main/java/al/demo/alarmmanagerdemo/Game.java
@@ -10,6 +10,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.app.Service;
@@ -22,44 +23,19 @@ import android.content.ComponentName;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-import al.demo.alarmmanagerdemo.AlarmPlayer.LocalBinder;
 
-public class Game extends Activity {
+public class Game extends AppCompatActivity {
     private Context context;
-    private AlarmPlayer alarmPlayer;
-    private boolean mBound = false;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
-
-
-     private ServiceConnection mConnection = new ServiceConnection() {
-
-
-        @Override
-        public void onServiceConnected(ComponentName className,
-                IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            LocalBinder binder = (LocalBinder) service;
-            alarmPlayer = binder.GetAlarmPlayer();
-            mBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
 
     public void gameCompleted()
     {
         setContentView(R.layout.game_completed);
         stopService(new Intent(this, AlarmPlayer.class));
-
-
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,11 +49,4 @@ public class Game extends Activity {
     {
         this.finish();
     }
-
-    protected void onStop() {
-        super.onStop();
-        unbindService(mConnection);
-        mBound = false;
-    }
-
 }
