cd8e9e77f1333f6fb04099dede8f2ecff5039a49
patsimd <>
2018-04-02 15:59:47 -0400

    g

--- app/src/main/java/al/demo/alarmmanagerdemo/Game.java
+++ app/src/main/java/al/demo/alarmmanagerdemo/Game.java
@@ -2,8 +2,9 @@ package al.demo.alarmmanagerdemo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 
-public class Game extends Activity {
+public class Game extends AppCompatActivity {
     Context context;
 
     public void gameCompleted()
