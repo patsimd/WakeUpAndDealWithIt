e23448a31f4fa9b3adb94589d86741184fea9572
patsimd <>
2018-04-02 17:19:36 -0400

    On peut choisir la sonnerie

--- app/src/main/java/al/demo/alarmmanagerdemo/fragment/setAlarmFragment.java
+++ app/src/main/java/al/demo/alarmmanagerdemo/fragment/setAlarmFragment.java
@@ -3,6 +3,8 @@ package al.demo.alarmmanagerdemo.fragment;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -20,6 +22,8 @@ import al.demo.alarmmanagerdemo.DatabaseHelper;
 import al.demo.alarmmanagerdemo.MainActivity;
 import al.demo.alarmmanagerdemo.R;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * Created by Utilisateur on 2018-04-01.
  */
@@ -35,6 +39,7 @@ public class setAlarmFragment extends Fragment{
     private int s_hour;
     private int s_minute;
     private Calendar selectedTime;
+    private Uri alarmMusic;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle bundle){
         View v = inflater.inflate(R.layout.set_alarm,vg,false);
@@ -47,6 +52,7 @@ public class setAlarmFragment extends Fragment{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
         alarmHelper = new AlarmHelper(this.getContext());
+        alarmMusic = RingtoneManager.getActualDefaultRingtoneUri(getContext(), RingtoneManager.TYPE_ALARM);
 
         alarmStatusTextView = (TextView) view.findViewById(R.id.status_text_view);
         alarmNameTextView = (TextView)view.findViewById(R.id.alarmName);
@@ -59,6 +65,8 @@ public class setAlarmFragment extends Fragment{
                     notificationIntent.putExtra("AlarmName","Alarm!");
                 else
                     notificationIntent.putExtra("AlarmName",alarmNameTextView.getText().toString());
+                notificationIntent.putExtra("musicUri",alarmMusic.toString());
+
                 long id = MainActivity.dbHelper.addAlarm(alarmNameTextView.getText().toString(),Calendar.getInstance().getTimeInMillis() + 10,1,"easy");
                 PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), (int)id, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
                 //selectedTime = Calendar.getInstance();
@@ -91,6 +99,30 @@ public class setAlarmFragment extends Fragment{
             }
         });
 
+
+        view.findViewById(R.id.ButtonChooseMusic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALARM);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI,  alarmMusic);
+                startActivityForResult(intent, 10);
+            }
+        });
+
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode == RESULT_OK && requestCode == 10){
+            Uri tempUri = alarmMusic = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+            if(tempUri != null)
+                alarmMusic = tempUri;
+        }
+    }
 }
