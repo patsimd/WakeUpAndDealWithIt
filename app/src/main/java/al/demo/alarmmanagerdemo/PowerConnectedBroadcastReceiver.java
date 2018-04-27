6e89bf4c9e12fcb78575e8f27d00378db2205212
patsimd <>
2018-04-27 17:16:20 -0400

    Javais oublié un if fucking important

--- app/src/main/java/al/demo/alarmmanagerdemo/PowerConnectedBroadcastReceiver.java
+++ app/src/main/java/al/demo/alarmmanagerdemo/PowerConnectedBroadcastReceiver.java
@@ -27,24 +27,27 @@ public class PowerConnectedBroadcastReceiver extends BroadcastReceiver {
 
                 while (allAlarmsCursor.moveToNext()) {
 
-                    Intent notificationIntent = new Intent("al.demo.alarmmanagerdemo.NOTIFY_ACTION");
-                    notificationIntent.putExtra("AlarmName", allAlarmsCursor.getString(1));
-                    notificationIntent.putExtra("musicUri", allAlarmsCursor.getString(7));
-                    notificationIntent.putExtra("difficultyString", allAlarmsCursor.getString(5));
-                    notificationIntent.putExtra("alarmID", allAlarmsCursor.getInt(0));
+                    if (allAlarmsCursor.getInt(4) == 1) { //Enabled
 
-                    PendingIntent pendingIntent = PendingIntent.getBroadcast(context,  allAlarmsCursor.getInt(0), notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        Intent notificationIntent = new Intent("al.demo.alarmmanagerdemo.NOTIFY_ACTION");
+                        notificationIntent.putExtra("AlarmName", allAlarmsCursor.getString(1));
+                        notificationIntent.putExtra("musicUri", allAlarmsCursor.getString(7));
+                        notificationIntent.putExtra("difficultyString", allAlarmsCursor.getString(5));
+                        notificationIntent.putExtra("alarmID", allAlarmsCursor.getInt(0));
 
-                    Calendar selectedTime = Calendar.getInstance();
-                    selectedTime.set(Calendar.MINUTE, allAlarmsCursor.getInt(3));
-                    selectedTime.set(Calendar.HOUR_OF_DAY, allAlarmsCursor.getInt(2));
-                    selectedTime.set(Calendar.SECOND, 0);
-                    if (selectedTime.before(Calendar.getInstance())) {
-                        if(!(selectedTime.get(Calendar.HOUR_OF_DAY) == Calendar.getInstance().get(Calendar.HOUR_OF_DAY) && selectedTime.get(Calendar.MINUTE) == Calendar.getInstance().get(Calendar.MINUTE)))
-                            selectedTime.add(Calendar.DAY_OF_MONTH, 1);
+                        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, allAlarmsCursor.getInt(0), notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                        Calendar selectedTime = Calendar.getInstance();
+                        selectedTime.set(Calendar.MINUTE, allAlarmsCursor.getInt(3));
+                        selectedTime.set(Calendar.HOUR_OF_DAY, allAlarmsCursor.getInt(2));
+                        selectedTime.set(Calendar.SECOND, 0);
+                        if (selectedTime.before(Calendar.getInstance())) {
+                            if (!(selectedTime.get(Calendar.HOUR_OF_DAY) == Calendar.getInstance().get(Calendar.HOUR_OF_DAY) && selectedTime.get(Calendar.MINUTE) == Calendar.getInstance().get(Calendar.MINUTE)))
+                                selectedTime.add(Calendar.DAY_OF_MONTH, 1);
+                        }
+
+                        alarmManager.schedulePendingIntent(selectedTime.getTimeInMillis(), pendingIntent);
                     }
-
-                    alarmManager.schedulePendingIntent(selectedTime.getTimeInMillis(), pendingIntent);
                 }
 
                 allAlarmsCursor.close();
